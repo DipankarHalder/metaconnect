5ddf49aa4d534baff6bef6453f3d44f418f72502
dipankarhalder <webmail.dip@gmail.com>
2022-06-22 12:14:33 +0530

    completed eslint and pritter setup

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,7 +1,7 @@
-export function App() {
+export const App = () => {
   return (
     <div className="App">
-      <h1>Hello Code</h1>
+      <h1>Hello React &amp; Redux, ESlint, Pritter and so many options..</h1>
     </div>
   );
-}
+};
